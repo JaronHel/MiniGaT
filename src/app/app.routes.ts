e22f995307807710914d787c5d50f6863d8e2968
Jaron Hellenbrand <jaron.hellenbrand@zimmer-group.com>
2024-12-13 08:13:57 +0100

    Smaller tweaks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,52 +1,52 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GamesComponent } from './games/games.component';
-import { BlackjackComponent } from './games/blackjack/blackjack.component';
 import { ToolsComponent } from './tools/tools.component';
-import { PassgenComponent } from './tools/PassGen/passgen.component';
+import { BlackjackComponent } from './games/blackjack/blackjack.component';
 import { MemoryComponent } from './games/memory/memory.component';
 import { HolComponent } from './games/higherorlower/hol.component';
+import { PassgenComponent } from './tools/PassGen/passgen.component';
 import { ColorConverterComponent } from './tools/color-converter/color-converter.component';
 import { QRGenComponent } from './tools/QRGen/qrgen.component';
 
 export const routes: Routes = [
   {
-    path: '', redirectTo: '/home',  pathMatch: 'full'
+    path: '', redirectTo: '/home', pathMatch: 'full'
   },
   {
     path: 'home',
-    component: HomeComponent,
+    component: HomeComponent
   },
   {
     path: 'games',
-    component: GamesComponent,
+    component: GamesComponent
   },
   {
     path: 'tools',
-    component: ToolsComponent,
+    component: ToolsComponent
   },
   {
     path: 'games/blackjack',
-    component: BlackjackComponent,
+    component: BlackjackComponent
   },
   {
     path: 'games/memory',
-    component: MemoryComponent,
+    component: MemoryComponent
   },
   {
     path: 'games/higherorlower',
-    component: HolComponent,
+    component: HolComponent
   },
   {
     path: 'tools/passgen',
-    component: PassgenComponent,
+    component: PassgenComponent
   },
   {
     path: 'tools/colorconverter',
-    component: ColorConverterComponent,
+    component: ColorConverterComponent
   },
   {
     path: 'tools/qrgen',
-    component: QRGenComponent,
+    component: QRGenComponent
   }
 ];
