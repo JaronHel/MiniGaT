99cd8c06bea3a21f9ba63b39ad7a0a66fb0e54bd
JaronHel <jaronhellenbrand07@gmail.com>
2024-12-14 19:50:57 +0100

    little style rework, added calculator to tools without function yet.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,52 +6,80 @@ import { MemoryComponent } from './games/memory/memory.component';
 import { HolComponent } from './games/higherorlower/hol.component';
 import { SlotsComponent } from './games/slots/slots.component';
 import { ToolsComponent } from './tools/tools.component';
-import { PassgenComponent } from './tools/PassGen/passgen.component';
+import { PassgenComponent } from './tools/passgen/passgen.component';
 import { ColorConverterComponent } from './tools/color-converter/color-converter.component';
-import { QRGenComponent } from './tools/QRGen/qrgen.component';
+import { QRGenComponent } from './tools/qrgen/qrgen.component';
+import { CalculatorComponent } from './tools/calculator/calculator.component';
 
 export const routes: Routes = [
+  // -------------------------------------- Default
   {
     path: '', redirectTo: '/home', pathMatch: 'full'
   },
+
+  // -------------------------------------- Home
   {
     path: 'home',
     component: HomeComponent
   },
+
+  // -------------------------------------- Games
   {
     path: 'games',
     component: GamesComponent
   },
+
+  // Blackjack
   {
     path: 'games/blackjack',
     component: BlackjackComponent
   },
+
+  // Memory
   {
     path: 'games/memory',
     component: MemoryComponent
   },
+
+  // Higher or Lower
   {
     path: 'games/higherorlower',
     component: HolComponent
   },
+
+  // Slots
   {
     path: 'games/slots',
     component: SlotsComponent
   },
+
+  // -------------------------------------- Tools
   {
     path: 'tools',
     component: ToolsComponent
   },
+
+  // PassGen
   {
     path: 'tools/passgen',
     component: PassgenComponent
   },
+
+  // Color Converter
   {
     path: 'tools/colorconverter',
     component: ColorConverterComponent
   },
+
+  // QRGen
   {
     path: 'tools/qrgen',
     component: QRGenComponent
+  },
+
+  // Calculator
+  {
+    path: 'tools/calculator',
+    component: CalculatorComponent
   }
 ];
