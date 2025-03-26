eca22326f46be32be2b79681e9cb18632f9d06ee
Jaron Hellenbrand <jaron.hellenbrand@zimmer-group.com>
2025-03-26 15:21:34 +0100

    changed file names and updated version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,72 +12,60 @@ import { QRGenComponent } from './tools/qrgen/qrgen.component';
 import { CalculatorComponent } from './tools/calculator/calculator.component';
 
 export const routes: Routes = [
-  // -------------------------------------- Default
   {
     path: '', redirectTo: '/home', pathMatch: 'full'
   },
 
-  // -------------------------------------- Home
   {
     path: 'home',
     component: HomeComponent
   },
 
-  // -------------------------------------- Games
   {
     path: 'games',
     component: GamesComponent
   },
 
-  // Blackjack
   {
     path: 'games/blackjack',
     component: BlackjackComponent
   },
 
-  // Memory
   {
     path: 'games/memory',
     component: MemoryComponent
   },
 
-  // Higher or Lower
   {
     path: 'games/higherorlower',
     component: HolComponent
   },
 
-  // Slots
   {
     path: 'games/slots',
     component: SlotsComponent
   },
 
-  // -------------------------------------- Tools
   {
     path: 'tools',
     component: ToolsComponent
   },
 
-  // PassGen
   {
     path: 'tools/passgen',
     component: PassgenComponent
   },
 
-  // Color Converter
   {
     path: 'tools/colorconverter',
     component: ColorConverterComponent
   },
 
-  // QRGen
   {
     path: 'tools/qrgen',
     component: QRGenComponent
   },
 
-  // Calculator
   {
     path: 'tools/calculator',
     component: CalculatorComponent
